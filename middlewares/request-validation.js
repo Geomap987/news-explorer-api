592c7b6aca8eea29e85f49b9da735ccc38a12bad
Geomap987 <buziuka@yandex.ru>
2020-12-16 22:20:37 +0300

    API request fixed

--- middlewares/request-validation.js
+++ middlewares/request-validation.js
@@ -3,9 +3,6 @@ const { Joi } = require('celebrate');
 
 const createUserRequest = {
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
